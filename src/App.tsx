946e44efbeb44ea769ec168e811d01d5a9bb0446
danielmpadua <danielmarianopadua@gmail.com>
2023-06-04 16:19:21 -0300

    change header images and dinamic favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { ThemeProvider } from "@mui/material/styles";
 
+import GreenFavicon from "./assets/images/danifavicongreen.png";
+import PurpleFavicon from "./assets/images/danifaviconpurple.png";
+import YellowFavicon from "./assets/images/danifaviconyellow.png";
 import { Presentation } from "./components";
 import { CustomTheme } from "./styles";
 
@@ -11,6 +14,24 @@ const App = () => {
 	const changeOption = (option: string) => {
 		setSelectedOption(option);
 	};
+
+	useEffect(() => {
+		const faviconUpdate = async () => {
+			const favicon = document.getElementById("favicon");
+
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			// @ts-ignore
+			if (selectedOption === "CODE") favicon.href = GreenFavicon;
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			// @ts-ignore
+			else if (selectedOption === "MUSIC") favicon.href = YellowFavicon;
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			// @ts-ignore
+			else favicon.href = PurpleFavicon;
+		};
+
+		faviconUpdate();
+	}, [selectedOption]);
 
 	return (
 		<ThemeProvider theme={CustomTheme()}>
